f5c42d7992d7b8e8157e9d7939608d473577718d
yubik nakarmi <yubik@email.com>
2022-01-07 22:07:04 +0545

    idk

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import  database
+from pymongo import MongoClient
+import certifi
+from . import views
+
+
+ca = certifi.where()
+client = MongoClient("mongodb+srv://yubik:12345@cluster0.cklcq.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
+db = client["database"]#acess of current databaser
+collist = db.list_collection_names()#finding collection list
+login = bool()
 
 # Create your views here.
 def hello(request):
@@ -8,5 +19,18 @@ def hello(request):
 def action_page(request):
     username = request.POST.get('uname')
     pw = request.POST.get('psw')
+    if username in collist:
+        col = db[username]
+        find = col.find({})
+        for find in find:
+            pw = find["password"]
+        if pw == pw:
+            return render(request, 'action_page.html', {'pw': pw})
 
-    return render(request, 'action_page.html',{'pw':pw})
+    else:
+        return render(request, 'notlign.html')
+
+
+
+
+
